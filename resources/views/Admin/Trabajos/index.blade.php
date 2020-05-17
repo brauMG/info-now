4e8e4b13857958971799641f3434f0a1cf40dc6a
bfmg0 <bfmg.08@outlook.es>
2020-05-17 14:13:50 -0700

    menu changes

--- resources/views/Admin/Trabajos/index.blade.php
+++ resources/views/Admin/Trabajos/index.blade.php
@@ -8,11 +8,6 @@
         <main role="main" class="ml-sm-auto">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2 h2-less">Trabajo</h1>
-                <div class="btn-toolbar mb-2 mb-md-0">
-                    <div class="btn-group mr-2">
-                        <button type="button" class="btn-less btn btn-info" id="new" onclick="AddJob();"><i class="fas fa-plus"></i> Agregar Trabajo</button>
-                    </div>
-                </div>
             </div>
         </main>
         <div id="Alert"></div>
