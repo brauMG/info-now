24cb0f2634df1bc0bbe2fbf9f3abd2c4ccac8a18
Braulio Martinez <bfmg.08@outlook.es>
2021-06-26 18:39:56 -0700

    removed fields from the pdf activities summary

--- app/Proyecto.php
+++ app/Proyecto.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Proyecto extends Model
 {
     //
-    protected $fillable = ['Clave_Compania','Descripcion','Clave_Status','Clave_Area','Clave_Fase','Clave_Enfoque','Clave_Trabajo','Clave_Indicador','Objectivo','FechaCreacion','Activo'];
+    protected $fillable = ['Clave_Compania','Descripcion','Criterio','Clave_Status','Clave_Area','Clave_Fase','Clave_Enfoque','Clave_Trabajo','Clave_Indicador','Objectivo','FechaCreacion','Activo'];
     protected $guarded = ['Clave'];
     protected $primaryKey = 'Clave';
     protected $table = 'Proyectos';
